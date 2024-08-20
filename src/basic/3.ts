6700fcac27b0f7c9269d6fda1d8d32bda6a174d5
Vikasa2704 <vikasa2704@gmail.com>
2024-08-20 22:45:43 +0300

    basic

---
+++ src/basic/3.ts
@@ -0,0 +1,9 @@
+//Створіть змінну, яка може містити або рядок, або число (union type)? Також, оголосіть змінну, яка може містити лише одне з двох можливих рядкових значень: 'enable' або 'disable' (literal type).
+
+
+
+//Union Types
+let value: 'string' | 'number';
+
+//literal type
+let status: 'enable' | 'disable';
